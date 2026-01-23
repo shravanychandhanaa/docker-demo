bed2a010916d8244c33388d92dbae3764a1add7a
Shravani Devarakonda <shravanychandhanaa28@gmail.com>
2026-01-23 10:09:07 +0530

    first commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Docker and Pipeline using GitHub Actions!\n');
+  res.end('Hello from Docker and GitHub Actions!\n');
 });
 
 server.listen(port, '0.0.0.0', () => {
